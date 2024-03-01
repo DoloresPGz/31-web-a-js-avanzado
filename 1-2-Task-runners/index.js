c72aff1e77ac7c63064d065da5fcfb6c2d138395
DoloresPGz <dolores.pgz@gmail.com>
2024-02-29 19:55:09 -0600

    clase 2 task runner y esm

---
+++ 1-2-Task-runners/index.js
@@ -0,0 +1,13 @@
+//importando una dependencia usando ecma script
+import pc from "picocolors"
+
+//importando una dependencia usando common js
+//const pc = require("picocolors")
+
+console.log(
+  pc.blue(`How are ${pc.italic(`you`)} doing?`)
+)
+
+console.log(
+    pc.green(`Hola mundo`)
+  )
