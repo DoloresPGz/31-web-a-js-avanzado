8cc3d92de5bd777d313a2bb0d5f0a431d73c4077
DoloresPGz <dolores.pgz@gmail.com>
2024-03-14 20:49:01 -0600

    base de la consulta de items

--- 1-6-CRUD/main.js
+++ 1-6-CRUD/main.js
@@ -1,3 +1,26 @@
-import APIKEY from "apikey.js";
+import APIKEY from "./apikey.js";
 
 const apiURL = `https://crudcrud.com/api/${APIKEY}/items`
+const nameInput = document.getElementById("article")
+const priceInput = document.getElementById("price")
+const itemContainer = document.getElementById("article-container")
+const itemForm = document.getElementById("crud-form")
+
+
+//CONSULTA DE LOS ITEMS
+
+function loadItems() {
+    
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(items => {
+        console.log("respuesta", items)
+    })
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadItems()
+})
